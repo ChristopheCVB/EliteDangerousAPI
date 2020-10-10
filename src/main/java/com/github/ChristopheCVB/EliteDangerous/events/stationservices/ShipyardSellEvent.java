14a07a1df7751c4716bbf8a3219a93e5a11120d8
Christophe Carvalho Vilas-Boas <christophe.carvalhovilasboas@gmail.com>
2020-10-10 23:33:39 +0200

    core: Refactor Gson Parsing Travel StationServices (#6)
    Co-authored-by: Christophe Carvalho Vilas-Boas <christophe.carvalhovilasboas@bobler.com>

--- src/main/java/com/github/ChristopheCVB/EliteDangerous/events/stationservices/ShipyardSellEvent.java
+++ src/main/java/com/github/ChristopheCVB/EliteDangerous/events/stationservices/ShipyardSellEvent.java
@@ -1,46 +1,13 @@
 package com.github.ChristopheCVB.EliteDangerous.events.stationservices;
 
 import com.github.ChristopheCVB.EliteDangerous.events.Event;
-import com.github.ChristopheCVB.EliteDangerous.utils.GameFilesUtils;
-import com.github.ChristopheCVB.EliteDangerous.utils.JsonUtils;
-import com.google.gson.JsonObject;
 
 public class ShipyardSellEvent extends Event {
+	public String shipType, shipTypeLocalised;
+	public Integer sellShipID;
+	public Long shipPrice, marketID;
 	
-	private String shipType, shipTypeLocalised;
-	private Integer sellShipId;
-	private Long shipPrice, marketId;
-	
-	public ShipyardSellEvent(String timestamp, JsonObject jsonEvent) {
+	public ShipyardSellEvent(String timestamp) {
         super(timestamp);
-
-        this.shipType = JsonUtils.pullString(jsonEvent, "ShipType");
-        this.shipTypeLocalised = JsonUtils.pullString(jsonEvent, "ShipType_Localised");
-        this.sellShipId = JsonUtils.pullInt(jsonEvent, "SellShipID");
-        this.shipPrice = JsonUtils.pullLong(jsonEvent, "ShipPrice");
-        this.marketId = JsonUtils.pullLong(jsonEvent, "MarketID");
-
-        GameFilesUtils.isAllEventDataProcessed(this, jsonEvent);
     }
-
-	public String getShipType() {
-		return shipType;
-	}
-
-	public String getShipTypeLocalised() {
-		return shipTypeLocalised;
-	}
-
-	public Integer getSellShipId() {
-		return sellShipId;
-	}
-
-	public Long getShipPrice() {
-		return shipPrice;
-	}
-
-	public Long getMarketId() {
-		return marketId;
-	}
-
 }
--- src/main/java/com/github/ChristopheCVB/EliteDangerous/events/stationservices/ShipyardSwapEvent.java
+++ src/main/java/com/github/ChristopheCVB/EliteDangerous/events/stationservices/ShipyardSwapEvent.java
@@ -1,51 +1,13 @@
 package com.github.ChristopheCVB.EliteDangerous.events.stationservices;
 
 import com.github.ChristopheCVB.EliteDangerous.events.Event;
-import com.github.ChristopheCVB.EliteDangerous.utils.GameFilesUtils;
-import com.github.ChristopheCVB.EliteDangerous.utils.JsonUtils;
-import com.google.gson.JsonObject;
 
 public class ShipyardSwapEvent extends Event {
+	public String shipType, shipTypeLocalised, storeOldShip;
+	public Integer shipID, storeShipID;
+	public Long marketID;
 	
-	private String shipType, shipTypeLocalised, storeOldShip;
-	private Integer shipId, storeShipId;
-	private Long marketId;
-	
-	public ShipyardSwapEvent(String timestamp, JsonObject jsonEvent) {
+	public ShipyardSwapEvent(String timestamp) {
         super(timestamp);
-
-        this.shipType = JsonUtils.pullString(jsonEvent, "ShipType");
-        this.shipTypeLocalised = JsonUtils.pullString(jsonEvent, "ShipType_Localised");
-        this.storeOldShip = JsonUtils.pullString(jsonEvent, "StoreOldShip");
-        this.shipId = JsonUtils.pullInt(jsonEvent, "ShipID");
-        this.storeShipId = JsonUtils.pullInt(jsonEvent, "StoreShipID");
-        this.marketId = JsonUtils.pullLong(jsonEvent, "MarketID");
-
-        GameFilesUtils.isAllEventDataProcessed(this, jsonEvent);
     }
-
-	public String getShipType() {
-		return shipType;
-	}
-
-	public String getShipTypeLocalised() {
-		return shipTypeLocalised;
-	}
-
-	public String getStoreOldShip() {
-		return storeOldShip;
-	}
-
-	public Integer getShipId() {
-		return shipId;
-	}
-
-	public Integer getStoreShipId() {
-		return storeShipId;
-	}
-
-	public Long getMarketId() {
-		return marketId;
-	}
-
 }
