3df485de05be17825cae8239941f0ed6a8047186
Pjiesco <Pjiesco@gmail.com>
2020-10-10 19:25:40 +0200

    core: (WIP) gson refactor parsing

--- src/main/java/com/github/ChristopheCVB/EliteDangerous/events/trade/BuyTradeDataEvent.java
+++ src/main/java/com/github/ChristopheCVB/EliteDangerous/events/trade/BuyTradeDataEvent.java
@@ -1,30 +1,13 @@
 package com.github.ChristopheCVB.EliteDangerous.events.trade;
 
 import com.github.ChristopheCVB.EliteDangerous.events.Event;
-import com.github.ChristopheCVB.EliteDangerous.utils.GameFilesUtils;
-import com.github.ChristopheCVB.EliteDangerous.utils.JsonUtils;
-import com.google.gson.JsonObject;
 
 public class BuyTradeDataEvent extends Event {
 	
-	private String system;
-	private Integer cost;
-	
-	public BuyTradeDataEvent(String timestamp, JsonObject jsonEvent) {
+	public String system;
+	public Integer cost;
+
+	public BuyTradeDataEvent(String timestamp) {
         super(timestamp);
-
-        this.system = JsonUtils.pullString(jsonEvent, "System");
-        this.cost = JsonUtils.pullInt(jsonEvent, "Cost");
-
-        GameFilesUtils.isAllEventDataProcessed(this, jsonEvent);
     }
-
-	public String getSystem() {
-		return system;
-	}
-
-	public Integer getCost() {
-		return cost;
-	}
-	
 }
--- src/main/java/com/github/ChristopheCVB/EliteDangerous/events/trade/CollectCargoEvent.java
+++ src/main/java/com/github/ChristopheCVB/EliteDangerous/events/trade/CollectCargoEvent.java
@@ -1,36 +1,14 @@
 package com.github.ChristopheCVB.EliteDangerous.events.trade;
 
 import com.github.ChristopheCVB.EliteDangerous.events.Event;
-import com.github.ChristopheCVB.EliteDangerous.utils.GameFilesUtils;
-import com.github.ChristopheCVB.EliteDangerous.utils.JsonUtils;
-import com.google.gson.JsonObject;
 
 public class CollectCargoEvent extends Event {
 	
-	private String type;
-	private Long missionID;
-	private Boolean stolen;
+	public String type;
+	public Long missionID;
+	public Boolean stolen;
 	
-	public CollectCargoEvent(String timestamp, JsonObject jsonEvent) {
+	public CollectCargoEvent(String timestamp) {
         super(timestamp);
-
-        this.type = JsonUtils.pullString(jsonEvent, "Type");
-        this.missionID = JsonUtils.pullLong(jsonEvent, "MissionID");
-        this.stolen = JsonUtils.pullBoolean(jsonEvent, "Stolen");
-
-        GameFilesUtils.isAllEventDataProcessed(this, jsonEvent);
     }
-
-	public String getType() {
-		return type;
-	}
-
-	public Long getMissionID() {
-		return missionID;
-	}
-
-	public Boolean getStolen() {
-		return stolen;
-	}
-
 }
