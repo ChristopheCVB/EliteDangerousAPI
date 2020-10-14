b04e0e1e6b89275248c2d9216786e54a758d64e8
Pjiesco <Pjiesco@gmail.com>
2020-10-14 14:26:52 +0200

    Fix: typo

--- src/main/java/com/github/ChristopheCVB/EliteDangerous/events/fleetcarriers/CarrierModulePackEvent.java
+++ src/main/java/com/github/ChristopheCVB/EliteDangerous/events/fleetcarriers/CarrierModulePackEvent.java
@@ -2,7 +2,7 @@ package com.github.ChristopheCVB.EliteDangerous.events.fleetcarriers;
 
 import com.github.ChristopheCVB.EliteDangerous.events.Event;
 
-public class CarrierModulePackEvent extends CarrierShipPackEvent {
+public class CarrierModulePackEvent extends CarrierPackEvent {
     public interface Listener extends Event.Listener {
         @Override
         default <T extends Event> void onTriggered(T event) {
