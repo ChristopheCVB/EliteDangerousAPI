f8c54dae56c79c7b8d719de3a3afa0e78f49580d
Christophe Carvalho Vilas-Boas <christophe.carvalhovilasboas@gmail.com>
2021-05-22 20:39:42 +0200

    fix: ProportionalItem

--- src/main/java/com/github/ChristopheCVB/EliteDangerous/models/ProportionalItem.java
+++ src/main/java/com/github/ChristopheCVB/EliteDangerous/models/ProportionalItem.java
@@ -1,4 +1,7 @@
 package com.github.ChristopheCVB.EliteDangerous.models;
 
 public abstract class ProportionalItem {
+    public String name;
+    public String nameLocalised;
+    public Float proportion;
 }
