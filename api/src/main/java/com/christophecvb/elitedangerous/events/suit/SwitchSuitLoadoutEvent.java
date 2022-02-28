c8033a5c1ad079f5865e27c0bcfd91062eb85f3c
Christophe Carvalho Vilas-Boas <christophe.carvalhovilasboas@gmail.com>
2022-02-28 15:00:15 +0100

    feat: SwitchSuitLoadoutEvent.Listener

--- api/src/main/java/com/christophecvb/elitedangerous/events/suit/SwitchSuitLoadoutEvent.java
+++ api/src/main/java/com/christophecvb/elitedangerous/events/suit/SwitchSuitLoadoutEvent.java
@@ -1,9 +1,20 @@
 package com.christophecvb.elitedangerous.events.suit;
 
+import com.christophecvb.elitedangerous.events.Event;
 import com.christophecvb.elitedangerous.models.LoadoutModule;
 
 import java.util.ArrayList;
 
 public class SwitchSuitLoadoutEvent extends SuitLoadoutEvent {
     public ArrayList<LoadoutModule> modules;
+    public ArrayList<String> suitMods;
+
+    public interface Listener extends Event.Listener {
+        @Override
+        default <T extends Event> void onTriggered(T event) {
+            this.onSwitchSuitLoadoutEventTriggered((SwitchSuitLoadoutEvent) event);
+        }
+
+        void onSwitchSuitLoadoutEventTriggered(SwitchSuitLoadoutEvent vehicleSwitchEvent);
+    }
 }
